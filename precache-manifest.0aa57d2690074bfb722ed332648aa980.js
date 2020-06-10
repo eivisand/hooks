3dc38d4f2ed6b4dc5cfc6444851c2d4dc1a73af1
Eivind Sandstad <eisandst@microsoft.com>
2020-06-10 14:42:39 +0200

    Updates

---
+++ precache-manifest.0aa57d2690074bfb722ed332648aa980.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6fef17eb8849dc2049ef21e66e05aa",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "7385e4b6353d3e851e48",
+    "url": "/hooks/static/css/main.ccc0ca4f.chunk.css"
+  },
+  {
+    "revision": "28d87ea2325d5d4daf1c",
+    "url": "/hooks/static/js/2.8f218618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks/static/js/2.8f218618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7385e4b6353d3e851e48",
+    "url": "/hooks/static/js/main.f34bd1bd.chunk.js"
+  },
+  {
+    "revision": "0c08b2fa7913cb3fd924",
+    "url": "/hooks/static/js/runtime-main.846bf6ec.js"
+  },
+  {
+    "revision": "f69854c03d4d812f40afa08a3444378e",
+    "url": "/hooks/static/media/class component.f69854c0.png"
+  },
+  {
+    "revision": "bfebbb013ba44fb93b8e30e2580edad5",
+    "url": "/hooks/static/media/contextConsumer.bfebbb01.png"
+  },
+  {
+    "revision": "6b99c6c320828f51f0158778aa92838a",
+    "url": "/hooks/static/media/contextProvider.6b99c6c3.png"
+  },
+  {
+    "revision": "c823c8c0a116bdaf5d04cbd70412812b",
+    "url": "/hooks/static/media/customHook.c823c8c0.png"
+  },
+  {
+    "revision": "b0fbd051712f79095ec8a7910ad83e2c",
+    "url": "/hooks/static/media/dependencies.b0fbd051.png"
+  },
+  {
+    "revision": "d38d6e5becbffce728bfef4aa7b7243e",
+    "url": "/hooks/static/media/functional component.d38d6e5b.png"
+  },
+  {
+    "revision": "b4a27207fcef7e03d08fde3d76eb3749",
+    "url": "/hooks/static/media/local state.b4a27207.png"
+  },
+  {
+    "revision": "d652f4fffef2b0bb082ae23652eeb259",
+    "url": "/hooks/static/media/outer.d652f4ff.png"
+  },
+  {
+    "revision": "cf8ddf114218db7a5a6e39f5b4a39388",
+    "url": "/hooks/static/media/outerMutating.cf8ddf11.png"
+  },
+  {
+    "revision": "7cb5adff5104d470a729ac7b0648d7e6",
+    "url": "/hooks/static/media/preCustomHook.7cb5adff.png"
+  },
+  {
+    "revision": "e115158ef4833fad7935981d6cee972d",
+    "url": "/hooks/static/media/renderFunction.e115158e.png"
+  },
+  {
+    "revision": "9a3a6ab7091339cde6041795d66b54ee",
+    "url": "/hooks/static/media/reuseStateLogic.9a3a6ab7.png"
+  },
+  {
+    "revision": "d9ec18f61a3d023b7ca371695e58f5c2",
+    "url": "/hooks/static/media/thisthat.d9ec18f6.png"
+  },
+  {
+    "revision": "002a419c2ed4fb36db4705f9de095cdb",
+    "url": "/hooks/static/media/thisthisthis.002a419c.png"
+  },
+  {
+    "revision": "a6565e1d76b206d67b24b9ebedfeabf8",
+    "url": "/hooks/static/media/unReusableStateLogic.a6565e1d.png"
+  },
+  {
+    "revision": "e3b116e6bb76663e0b7e5cdc6c2735b9",
+    "url": "/hooks/static/media/useContext.e3b116e6.png"
+  },
+  {
+    "revision": "62f006453af393f9652fe5188569afee",
+    "url": "/hooks/static/media/useCustomHook.62f00645.png"
+  },
+  {
+    "revision": "51f155a58629f43d244444be5e4ff6d1",
+    "url": "/hooks/static/media/useMemoized.51f155a5.png"
+  },
+  {
+    "revision": "30fba85688c1f8226b22ca50f3e19586",
+    "url": "/hooks/static/media/useRef.30fba856.png"
+  },
+  {
+    "revision": "ac19f779026738fe35497ed82ea5e890",
+    "url": "/hooks/static/media/useRefComponent.ac19f779.png"
+  },
+  {
+    "revision": "29151fb149c8816c317c8c75dc192195",
+    "url": "/hooks/static/media/useState.29151fb1.png"
+  }
+]);
